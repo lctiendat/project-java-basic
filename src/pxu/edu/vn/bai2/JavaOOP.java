4aa3fcfb59d657bb7285ebf6e1c4747c24d6384c
lctiendat <55487768+lctiendat@users.noreply.github.com>
2023-06-30 14:02:13 +0700

    Thay doi lan 3

--- src/pxu/edu/vn/bai2/JavaOOP.java
+++ src/pxu/edu/vn/bai2/JavaOOP.java
@@ -1,10 +1,10 @@
-package pxu.edu.vn.bai1;
+package pxu.edu.vn.bai2;
 
-public class Hello {
+public class JavaOOP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+
 	}
 
 }
